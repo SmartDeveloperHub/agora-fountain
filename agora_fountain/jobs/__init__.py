3b1c067606b3914d118d08614eac5b696744b67f
Fernando Serena <fernando.serena@centeropenmiddleware.com>
2015-05-27 12:09:09 +0200

    #2 Using APScheduler for jobs management

--- agora_fountain/jobs/__init__.py
+++ agora_fountain/jobs/__init__.py
@@ -34,17 +34,17 @@ jobstores = {
     'default': RedisJobStore(db=2)
 }
 executors = {
-    'default': {'type': 'threadpool', 'max_workers': 20},
-    'processpool': ProcessPoolExecutor(max_workers=5)
+    'default': {'type': 'threadpool', 'max_workers': 20}
+    # 'processpool': ProcessPoolExecutor(max_workers=5)
 }
 job_defaults = {
-    'coalesce': False,
-    'max_instances': 3
+    # 'coalesce': False,
+    # 'max_instances': 3
 }
 scheduler = BackgroundScheduler()
 
 # .. do something else here, maybe add jobs etc.
 
-scheduler.configure(jobstores=jobstores, executors=executors, job_defaults=job_defaults, timezone=utc)
+scheduler.configure(jobstores=jobstores, executors=executors, job_defaults=job_defaults)
 
 scheduler.start()
