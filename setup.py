594602514d85098cf67588449565cafa0c9c1d0c
Fernando Serena <fernando.serena@centeropenmiddleware.com>
2015-06-17 08:45:26 +0200

    Updated to 0.2.2

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Agora-Fountain",
-    version="0.2.1",
+    version="0.2.2",
     author="Fernando Serena",
     author_email="fernando.serena@centeropenmiddleware.com",
     description="The Agora core service for ontology paths discovery and seed management",
