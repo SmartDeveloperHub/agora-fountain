0ac9e8c55dc1775ee4c5bb1e235fdae35a6429b5
fserena <fernando.serena@centeropenmiddleware.com>
2015-08-04 13:37:07 +0200

    #15 Added generic methods for testing

--- agora/fountain/test/__init__.py
+++ agora/fountain/test/__init__.py
@@ -2,6 +2,7 @@ __author__ = 'fernando'
 
 import unittest
 from agora.fountain.server import app
+from nose.tools import *
 
 
 def setup():
@@ -23,6 +24,27 @@ class FountainTest(unittest.TestCase):
 
     def tearDown(self):
         pass
+
+    def get(self, path, exp_code=200, message=None):
+        rv = self.app.get(path)
+        if message is None:
+            message = 'There is a problem with the request'
+        eq_(rv.status_code, exp_code, message)
+        return rv.data
+
+    def post(self, path, data, content_type='text/turtle', exp_code=201, message=None):
+        rv = self.app.post(path, data=data, headers={'Content-Type': content_type})
+        if message is None:
+            message = 'The resource was not created properly'
+        eq_(rv.status_code, exp_code, message)
+        return rv.data
+
+    def delete(self, path, error_message=None):
+        rv = self.app.delete(path)
+        if error_message is None:
+            error_message = "The resource couldn't be deleted"
+        eq_(rv.status_code, 200, error_message)
+        return rv.data
 
 
 def teardown():
