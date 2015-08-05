9ea843218c2463891eef84386aa468cc165c6be1
fserena <fernando.serena@centeropenmiddleware.com>
2015-08-05 12:42:12 +0200

    #15 Simple vocab fully tested

--- agora/fountain/test/__init__.py
+++ agora/fountain/test/__init__.py
@@ -27,6 +27,9 @@ __author__ = 'Fernando Serena'
 import unittest
 from agora.fountain.server import app
 from nose.tools import *
+import networkx as nx
+import json
+from functools import wraps
 
 
 def setup():
@@ -42,6 +45,41 @@ def setup():
     from agora.fountain import api
 
 
+class AgoraGraph(nx.DiGraph):
+    def __init__(self):
+        super(AgoraGraph, self).__init__()
+
+    @property
+    def types(self):
+        return [t for t, data in self.nodes(data=True) if data['ty'] == 'type']
+
+    @property
+    def properties(self):
+        return [t for t, data in self.nodes(data=True) if data['ty'] == 'prop']
+
+    def _check_node_type(self, f, ty):
+        def wrapper(name):
+            for n, data in self.nodes_iter(data=True):
+                if n == name:
+                    if data['ty'] == ty:
+                        return f(name)
+                    raise Exception(ty)
+            raise KeyError(ty)
+        return wrapper
+
+    def get_type_properties(self, ty):
+        return self._check_node_type(self.successors, 'type')(ty)
+
+    def get_type_refs(self, ty):
+        return self._check_node_type(self.predecessors, 'type')(ty)
+
+    def get_property_domain(self, prop):
+        return self._check_node_type(self.predecessors, 'prop')(prop)
+
+    def get_property_range(self, prop):
+        return self._check_node_type(self.successors, 'prop')(prop)
+
+
 class FountainTest(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
@@ -49,11 +87,11 @@ class FountainTest(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def get(self, path, exp_code=200, message=None):
+    def get(self, path, exp_code=200, error_message=None):
         rv = self.app.get(path)
-        if message is None:
-            message = 'There is a problem with the request'
-        eq_(rv.status_code, exp_code, message)
+        if error_message is None:
+            error_message = 'There is a problem with the request'
+        eq_(rv.status_code, exp_code, error_message)
         return rv.data
 
     def post(self, path, data, content_type='text/turtle', exp_code=201, message=None):
@@ -69,6 +107,44 @@ class FountainTest(unittest.TestCase):
             error_message = "The resource couldn't be deleted"
         eq_(rv.status_code, 200, error_message)
         return rv.data
+
+    @property
+    def types(self):
+        return json.loads(self.get('/types'))['types']
+
+    @property
+    def properties(self):
+        return json.loads(self.get('/properties'))['properties']
+
+    def get_type(self, ty):
+        return json.loads(self.get('/types/{}'.format(ty)))
+
+    def get_property(self, ty):
+        return json.loads(self.get('/properties/{}'.format(ty)))
+
+    @property
+    def graph(self):
+        graph = AgoraGraph()
+        types = self.types
+        graph.add_nodes_from(types, ty='type')
+        for node in self.properties:
+            p_dict = self.get_property(node)
+            dom = p_dict.get('domain')
+            ran = p_dict.get('range')
+            edges = [(d, node) for d in dom]
+            if p_dict.get('type') == 'object':
+                edges.extend([(node, r) for r in ran])
+            graph.add_edges_from(edges)
+            graph.add_node(node, ty='prop', object=p_dict.get('type') == 'object')
+        for node in types:
+            p_dict = self.get_type(node)
+            refs = p_dict.get('refs')
+            props = p_dict.get('properties')
+            edges = [(r, node) for r in refs]
+            edges.extend([(node, p) for p in props])
+            graph.add_edges_from(edges)
+
+        return graph
 
 
 def teardown():
---
+++ agora/fountain/test/simple_vocab_test.py
@@ -0,0 +1,96 @@
+"""
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+  This file is part of the Smart Developer Hub Project:
+    http://www.smartdeveloperhub.org
+
+  Center for Open Middleware
+        http://www.centeropenmiddleware.com/
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+  Copyright (C) 2015 Center for Open Middleware.
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+            http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+"""
+
+__author__ = 'Fernando Serena'
+
+from agora.fountain.test import FountainTest
+import json
+from nose.tools import *
+
+
+class DummyTest(FountainTest):
+    def a_test_empty_vocabs(self):
+        vocabs = json.loads(self.get('/vocabs'))
+        eq_(len(vocabs), False, 'Fountain should be empty')
+
+    def b_test_post_dummy_vocab(self):
+        with open('agora/fountain/test/vocabs/dummy.ttl') as f:
+            dummy_vocab = f.read()
+            self.post('/vocabs', dummy_vocab, message='The vocabulary was not created properly')
+
+    def c_test_contains_dummy(self):
+        vocabs = json.loads(self.get('/vocabs'))
+        eq_(len(vocabs), 1, 'Fountain should contain the dummy vocab')
+        assert 'test' in vocabs, 'The prefix of the contained vocabulary must be "test"'
+        vocab = self.get('/vocabs/test')
+        assert len(vocab), 'RDF must not be empty'
+
+    def c1_test_dummy_properties(self):
+        graph = self.graph
+        props = sorted(graph.properties)
+        eq_(len(props), 2, 'Fountain should contain two properties, but found: %s' % len(props))
+        assert 'test:prop1' == props.pop(0)
+        assert 'test:prop2' == props.pop()
+
+        # prop1
+        p1_domain = graph.get_property_domain('test:prop1')
+        eq_(len(p1_domain), 1, 'prop1 must have 1 domain type')
+        assert 'test:Concept1' in p1_domain
+        p1_range = graph.get_property_range('test:prop1')
+        eq_(len(p1_range), 1, 'prop1 must have 1 range type')
+        assert 'test:Concept2' in p1_range
+
+        # prop2
+        p2_domain = graph.get_property_domain('test:prop2')
+        eq_(len(p2_domain), 1, 'prop2 must have 1 domain type')
+        assert 'test:Concept2' in p2_domain
+        p2_range = graph.get_property_range('test:prop2')
+        eq_(len(p2_range), 1, 'prop2 must have 1 range type')
+        assert 'test:Concept1' in p2_range
+
+    def c1_test_dummy_types(self):
+        graph = self.graph
+        types = sorted(graph.types)
+        eq_(len(types), 2, 'Fountain should contain two types, but found: %s' % len(types))
+        assert 'test:Concept1' == types.pop(0)
+        assert 'test:Concept2' == types.pop()
+
+        # Concept 1
+        c1_properties = graph.get_type_properties('test:Concept1')
+        eq_(len(c1_properties), 1, 'Concept1 must have 1 property')
+        assert 'test:prop1' in c1_properties
+        c1_refs = graph.get_type_refs('test:Concept1')
+        eq_(len(c1_refs), 1, 'Concept1 must have 1 reference')
+        assert 'test:prop2' in c1_refs
+
+        # Concept 2
+        c2_properties = graph.get_type_properties('test:Concept2')
+        eq_(len(c2_properties), 1, 'Concept2 must have 1 property')
+        assert 'test:prop2' in c2_properties
+        c2_refs = graph.get_type_refs('test:Concept2')
+        eq_(len(c2_refs), 1, 'Concept2 must have 1 reference')
+        assert 'test:prop1' in c2_refs
+
+    def d_test_delete_dummy_vocab(self):
+        self.delete('/vocabs/test', 'The test vocabulary should exist previously')
