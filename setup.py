6b69fcc66c3aa328b4e79efb5b5d7f08a9b7652c
Fernando Serena <fernando.serena@centeropenmiddleware.com>
2015-09-03 13:33:10 +0200

    Added develop branch to travis

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Agora-Fountain",
-    version="0.3.0-dev0",
+    version="0.3.0-dev1",
     author="Fernando Serena",
     author_email="fernando.serena@centeropenmiddleware.com",
     description="The Agora core service for ontology paths discovery and seed management",
